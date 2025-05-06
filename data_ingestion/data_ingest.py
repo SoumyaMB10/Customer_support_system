0e8d78775496bd201f85ea8d7fb88ab6fb03df1e
SoumyaMB10 <mbsoumya10@gmail.com>
2025-05-06 14:11:43 +0100

    code updated data transform.py

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -1,5 +1,18 @@
-from langchain_astradb import AstraDBvectoerstore
+from langchain_astradb import AstraDBVectorStore
 from dotenv import load_dotenv
 import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
+
+## push the data into VDB
+class ingest_data:
+    def __init__(self):
+        print("data ingestion initiated....")
+        pass
+
+    def data_ingestion(self):
+        pass
+
+## to check if the current file is corect and working 
+if __name__ == '__main__':
+    data_ingestion = ingest_data()
--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from langchain_core.documents import Document
 
-
+# transform/load the data
 class data_converter:
     def __init__(self):
-        pass
+        print("data transformation initiated...")
+        self.product_data = pd.read_csv(r"C:\Users\HP\Desktop\Customer_support_system\data\flipkart_product_review.csv")
+        print(self.product_data.head())
         
     def data_transfomation(self):
         pass
+
+
+if __name__ == "__main__":
+    data_converter()
