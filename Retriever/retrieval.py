0e40d57924e87e3ad770b4fb5400a91a028ca253
SoumyaMB10 <mbsoumya10@gmail.com>
2025-05-07 09:46:24 +0100

    updated retriever and main.py

---
+++ Retriever/retrieval.py
@@ -0,0 +1,31 @@
+import os
+from langchain_astradb import AstraDBStore
+from typing import List
+from langchain_core.documents import Document
+from config.config_loaders import load_config
+from utils.model_loader import ModelLoader
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class Retriever:
+
+    def __init__(self):
+        pass
+
+    def _load_env_variables(self):
+        pass
+
+    def load_retriever(self):
+        pass
+
+    def call_retriever(self, query:str)->List[Document]:
+        pass
+
+if __name__ == '__main__':
+    retriever_obj = Retriever()
+    user_query = "can you suggest good budget laptop"
+    results = retriever_obj.call_retriever(user_query)
+
+    for idx, doc in enumerate(results,1):
+        print(f"Results {idx}: {doc.page_content}\n Metadata: {doc.metadata}\n")
