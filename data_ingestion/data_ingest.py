723211108b196ffa1f93f233797b68867f7afe3c
SoumyaMB10 <mbsoumya10@gmail.com>
2025-05-07 05:35:16 +0100

    updated api keys

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -4,6 +4,12 @@ import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
 
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["ASTRA_DB_API_ENDPOINT"] = os.getenv("ASTRA_DB_API_ENDPOINT")
+os.environ["ASTRA_DB_APPLICATION_TOKEN"] = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+os.environ["ASRA_DB_KEYSPACE"] = os.getenv("ASRA_DB_KEYSPACE")
+
 ## push the data into VDB
 class ingest_data:
     def __init__(self):
--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -4,13 +4,37 @@ from langchain_core.documents import Document
 # transform/load the data
 class data_converter:
     def __init__(self):
-        print("data transformation initiated...")
+        #print("data transformation initiated...")
         self.product_data = pd.read_csv(r"C:\Users\HP\Desktop\Customer_support_system\data\flipkart_product_review.csv")
-        print(self.product_data.head())
+        #print(self.product_data.head())
         
     def data_transfomation(self):
-        pass
+        required_col = self.product_data.columns[1:]
+        #print(required_col)
+        product_list = []
+        for index, row in self.product_data.iterrows():
+            object = {
+                "product_name" : row["product_title"],
+                "product_rating" : row["rating"],
+                "product_summary" : row["summary"],
+                "product_review" : row["review"],
+            }
+            product_list.append(object)
+        #print(product_list)
+        docs = []
+        for entry in product_list:
+            metadata = {"product_name":entry["product_name"],
+                        "product_rating":entry["product_rating"],
+                        "product_summary":entry["product_summary"]}
+            doc = Document(page_content=entry["product_review"], metadata = metadata)
+            docs.append(doc)
+
+        return docs
+
+
+
+
 
 
 if __name__ == "__main__":
-    data_converter()
+    data_transform = data_converter().data_transfomation()
