024d449080269de1d1ed30a98e9d93950ca03386
SoumyaMB10 <mbsoumya10@gmail.com>
2025-05-07 05:57:58 +0100

    data ingest code

--- data_ingestion/data_ingest.py
+++ data_ingestion/data_ingest.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from data_ingestion.data_transform import data_converter
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
@@ -14,10 +15,23 @@ os.environ["ASRA_DB_KEYSPACE"] = os.getenv("ASRA_DB_KEYSPACE")
 class ingest_data:
     def __init__(self):
         print("data ingestion initiated....")
-        pass
+        self.embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+        self.docs = data_converter.data_transfomation()
 
-    def data_ingestion(self):
-        pass
+    def data_ingestion(self, status):
+        vstore = AstraDBVectorStore(embedding=self.embeddings,
+                           collection_name="chatbotecom",
+                           api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"],
+                           token=os.environ["ASTRA_DB_APPLICATION_TOKEN"],
+                           namespace=os.environ["ASRA_DB_KEYSPACE"])
+        storage = status
+        if storage==None:
+            inserted_ids = vstore.add_documents(self.docs)
+            print(inserted_ids)
+        else:
+            return vstore
+        
+        return vstore, inserted_ids
 
 ## to check if the current file is corect and working 
 if __name__ == '__main__':
